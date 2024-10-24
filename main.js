ad0fe3bc25bcaa29e360fe61646cf180e321873c
MuzamilTahliil <muzamiltahliil1@gmail.com>
2024-10-24 21:08:53 +0300

    Updating HTML and css by adding Our logo and olso we add some pictures for hero sectio

--- main.js
+++ main.js
@@ -10,24 +10,26 @@ togle_button.addEventListener('click', function(){
   const mainContainer = document.querySelector('.main');
   
   const images = [
-      'https://raw.githubusercontent.com/code-banu/my-first-project/refs/heads/main/assets/images/pasta2.avif',
-      'https://images.unsplash.com/photo-1432139509613-5c4255815697?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZvb2R8ZW58MHx8MHx8fDA%3D',
-      'https://plus.unsplash.com/premium_photo-1675252369719-dd52bc69c3df?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZvb2R8ZW58MHx8MHx8fDA%3D'
+      'image1.png',
+      'image2.png',
+      'img_5.jpg',
+      'img_6.jpg'
   ];
   
   let currentIndex = 0;
-  
   function changeImage() {
       currentIndex = (currentIndex + 1) % images.length; 
       mainContainer.style.backgroundImage = `url(${images[currentIndex]})`;
   }
-  
+
   changeImage();
-  
   setInterval(changeImage, 3000);
    
 
+
+
 // Section four 
+
 let currentSlide = 0;
 const testimonials = document.querySelectorAll('.testimonial');
 const dots = document.querySelectorAll('.dot');
