cba246a8dae54031718f83944fc278bf8b0d40c9
ayaandhaqane <148534224+ayaandhaqane@users.noreply.github.com>
2024-10-28 10:16:00 +0300

    adding searching API JSON

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
-async function fetchRecipes() {
+
+async function fetchRecipes(query = '') {
     try {
-      const response = await fetch('recipes.json'); 
+      const response = await fetch('/recipes.json');
       const data = await response.json();
-      displayRecipes(data.recipes);
+  
+    
+      const filteredRecipes = data.recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(query.toLowerCase())
+      );
+  
+  
+      displayRecipes(filteredRecipes);
     } catch (error) {
       console.error('Error fetching recipes:', error);
     }
   }
   
+
   function displayRecipes(recipes) {
     const recipeContainer = document.getElementById('recipe-results');
     recipeContainer.innerHTML = '';
   
-    recipes.forEach(recipe => {
+    if (recipes.length === 0) {
+      recipeContainer.innerHTML = '<p class="no-results">Sorry, we don\'t have it.</p>';
+    } else {
+      recipes.forEach(recipe => {
         const recipeCard = `
-            <div class="recipe-card">
-                <img src="${recipe.image}" alt="${recipe.title}">
-                <h3>${recipe.title}</h3>
-                <p>${recipe.description}</p>
-            </div>
+          <div class="recipe-card">
+            <h3>${recipe.title}</h3>
+            <img src="${recipe.image}" alt="${recipe.title}" />
+            <p>${recipe.description}</p>
+          </div>
         `;
         recipeContainer.innerHTML += recipeCard;
-    });
-
+      });
+    }
   }
+  
 
+  function searchRecipe() {
+    const searchQuery = document.getElementById('searching').value;
+    fetchRecipes(searchQuery); 
+  }
+  
   
   fetchRecipes();
   
-  
-
-
